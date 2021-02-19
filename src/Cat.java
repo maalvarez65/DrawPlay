75a706b307b0eda26724b63fbffcb0bae8553d1e
Chris Wilhoite <chriswilhoite18@icloud.com>
2021-02-18 18:45:33 -0700

    Modified the mouth size (made it less wide and taller), made the eyes smaller and more narrow, changed the skin of the cat to pink and it's mouth to black, increased the size of the head to 200, shifted the head by 25 to the right (makes the cat look like it's looking left) and finally edited the text under the cat to say 'I'm a naked mole-cat >.<'

--- src/Cat.java
+++ src/Cat.java
@@ -14,8 +14,8 @@ public class Cat {
 	private static final int EYE_HEIGHT = 40;
 	private static final int EYE_WIDTH = 5;
 	// pick mouth height, width is based on head dimension
-	private static final int MOUTH_HEIGHT = 10;
-	private static final int MOUTH_WIDTH = HEAD_DIMENSION/2;
+	private static final int MOUTH_HEIGHT = 20;
+	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
 	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
@@ -24,10 +24,10 @@ public class Cat {
 	public void draw(Graphics g, int catX, int catY)
 	{
 		Graphics2D g2 = (Graphics2D) g;
-		int x=catX;
+		int x=catX + 25;
 		int y=catY;
 		// Draw the head
-		g2.setColor(Color.black);
+		g2.setColor(Color.pink);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
 		g2.setColor(Color.green);
@@ -37,13 +37,13 @@ public class Cat {
 		x += EYE_SEPARATION;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
-		g2.setColor(Color.pink);
-		x = catX + MOUTH_X;
-		y = catY + MOUTH_Y;
+		g2.setColor(Color.black);
+		x = catX + MOUTH_X - 5;
+		y = catY + MOUTH_Y + 10;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("I'm a naked mole-cat >.<", catX + 50, catY+HEAD_DIMENSION + 50);	
 	}
 }
