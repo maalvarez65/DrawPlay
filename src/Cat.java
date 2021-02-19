8eececfb10825f4ddad66e3a25ea37558728236d
Chris Wilhoite <chriswilhoite18@icloud.com>
2021-02-18 18:51:08 -0700

    Decided to add 'WOOF!' and take out '>.<'

--- src/Cat.java
+++ src/Cat.java
@@ -44,6 +44,6 @@ public class Cat {
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("I'm a naked mole-cat >.<", catX + 50, catY+HEAD_DIMENSION + 50);	
+		g2.drawString("WOOF! I'm a naked mole-cat", catX + 50, catY+HEAD_DIMENSION + 50);	
 	}
 }
